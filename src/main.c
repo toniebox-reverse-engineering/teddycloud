67a0d34b976ad42b5cb4fd164c2dfc062e3934a4
SciLor <scilor@users.noreply.github.com>
2023-07-10 19:09:03 +0000

    enable cloud api

--- src/main.c
+++ src/main.c
@@ -27,37 +27,40 @@ int_t main(int argc, char *argv[])
         return -1;
     }
 
-#if 0
-    TRACE_INFO("**********************************\r\n");
-    TRACE_INFO("***       Cloud API test       ***\r\n");
-    TRACE_INFO("**********************************\r\n");
-    TRACE_INFO("\r\n");
-
-    char *request = NULL;
-    char *hash = NULL;
-
-    if (argc < 2)
-    {
-        TRACE_ERROR("Usage: %s <request> [hash]\r\n", argv[0]);
-        return -1;
-    }
     if (argc > 1)
     {
-        request = argv[1];
-        TRACE_ERROR("Request URL: %s\r\n", request);
+        TRACE_INFO("**********************************\r\n");
+        TRACE_INFO("***       Cloud API test       ***\r\n");
+        TRACE_INFO("**********************************\r\n");
+        TRACE_INFO("\r\n");
+
+        char *request = NULL;
+        char *hash = NULL;
+
+        if (argc < 2)
+        {
+            TRACE_ERROR("Usage: %s <request> [hash]\r\n", argv[0]);
+            return -1;
+        }
+        if (argc > 1)
+        {
+            request = argv[1];
+            TRACE_ERROR("Request URL: %s\r\n", request);
+        }
+        if (argc > 2)
+        {
+            hash = argv[2];
+            TRACE_ERROR("Hash: %s\r\n", hash);
+        }
+
+        TRACE_INFO("\r\n");
+
+        error = cloud_request_get(NULL, 0, request, hash, NULL);
     }
-    if (argc > 2)
+    else
     {
-        hash = argv[2];
-        TRACE_ERROR("Hash: %s\r\n", hash);
+        server_init();
     }
-
-    TRACE_INFO("\r\n");
-
-    error = cloud_request_get(NULL, 0, request, hash);
-#else
-    server_init();
-#endif
 
     tls_adapter_deinit();
     platform_deinit();
