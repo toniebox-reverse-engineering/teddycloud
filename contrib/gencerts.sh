fc18283453d087bd44d902564b58d9fad9c88767
SciLor <scilor@users.noreply.github.com>
2023-07-10 16:27:48 +0000

    fix certificate generation

--- contrib/gencerts.sh
+++ contrib/gencerts.sh
@@ -12,9 +12,9 @@ DAYS="9000"
 #faketime '2015-11-04 00:00:00' 
 
 mkdir -p certs/server
-openssl genrsa -out ${CA_KEY} 4096
-openssl req -x509 -new -nodes -extensions v3_ca -key ${CA_KEY} -days ${DAYS} -out ${CA_CRT} -sha512 -subj '/C=DE/CN=Teddy CA'
-openssl genrsa -out ${SRV_KEY} 4096
-openssl req -new -key ${SRV_KEY} -out ${SRV_CSR} -sha512 -subj '/C=DE/CN=Teddy Cloud'
-openssl x509 -req -in ${SRV_CSR} -CA ${CA_CRT} -CAkey ${CA_KEY} -CAcreateserial -out ${SRV_CRT} -days ${DAYS} -sha512
+openssl genrsa -out ${CA_KEY} 2048
+openssl req -x509 -new -nodes -extensions v3_ca -key ${CA_KEY} -days ${DAYS} -out ${CA_CRT} -sha256 -subj '/C=DE/CN=Teddy CA'
+openssl genrsa -out ${SRV_KEY} 2048
+openssl req -new -key ${SRV_KEY} -out ${SRV_CSR} -sha256 -subj '/C=DE/CN=TeddyCloud'
+openssl x509 -req -in ${SRV_CSR} -CA ${CA_CRT} -CAkey ${CA_KEY} -CAcreateserial -out ${SRV_CRT} -days ${DAYS} -sha256
 openssl x509 -inform PEM -outform DER -in ${CA_CRT} -out ${CA_CRT_DER}
