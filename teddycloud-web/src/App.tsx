505e9ee7d3b6ff5e5af1284f4eaf51a9e2af6e9c
Claudio Götz <goetz@netz.werk.ch>
2023-07-27 22:59:13 +0200

    setup translation management i18n

--- teddycloud-web/src/App.tsx
+++ teddycloud-web/src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 import { DatePicker } from "antd";
 
+import { useTranslation } from "react-i18next";
+
 function App() {
+  const { t } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
-        <p>Main page.</p>
+        <p>{t(`welcome.title`)}</p>
+        <p>Sample datepicker from ant design:</p>
         <DatePicker />
       </header>
     </div>
--- teddycloud-web/src/index.tsx
+++ teddycloud-web/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./i18n";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
