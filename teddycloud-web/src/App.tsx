c78194748cdf46dbd2faebb1a7373564ce06967b
Claudio Götz <goetz@netz.werk.ch>
2023-07-27 22:40:05 +0200

    init teddycloud-web
    antd for design elements
    formik for forms
    yup for form validation schemas
    styled-components for css
    i18next for translations / multilanguage

---
+++ teddycloud-web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { DatePicker } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Main page.</p>
+        <DatePicker />
+      </header>
+    </div>
+  );
+}
+
+export default App;
