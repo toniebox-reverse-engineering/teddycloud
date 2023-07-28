45121abeccaf8934048082fba6fe407b54e6e0a0
Claudio Götz <goetz@netz.werk.ch>
2023-07-28 09:58:26 +0200

    example on stats page

--- teddycloud-web/src/pages/home/StatsPage.tsx
+++ teddycloud-web/src/pages/home/StatsPage.tsx
@@ -7,9 +7,26 @@ import {
   StyledSider,
 } from "../../components/StyledComponents";
 import { HomeSubNav } from "../../components/HomeSubNav";
+import { StatsItem, StatsList, TeddyCloudApi } from "../../api";
+import { defaultAPIConfig } from "../../config/defaultApiConfig";
+import { useEffect, useState } from "react";
+
+const api = new TeddyCloudApi(defaultAPIConfig());
 
 export const StatsPage = () => {
   const { t } = useTranslation();
+  const [stats, setStats] = useState<StatsList | undefined>();
+  useEffect(() => {
+    const fetchStats = async () => {
+      const statsRequest = (await api.statsGet()) as StatsList;
+      if (statsRequest?.stats?.length && statsRequest?.stats?.length > 0) {
+        setStats(statsRequest);
+        console.log("statsRequest", statsRequest);
+      }
+    };
+
+    fetchStats();
+  }, []);
 
   return (
     <>
