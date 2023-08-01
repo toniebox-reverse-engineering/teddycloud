8734588535e225ba2e6f511e7c78163ac6a355cd
SciLor <scilor@users.noreply.github.com>
2023-08-01 10:02:41 +0000

    Add filename to invalid TAF + don't warn on empty files

--- src/handler_cloud.c
+++ src/handler_cloud.c
@@ -259,17 +259,22 @@ tonie_info_t getTonieInfo(const char *contentPath)
                 }
                 else
                 {
-                    TRACE_WARNING("Invalid TAF-header, read_length=%" PRIuSIZE " != protobufSize=%" PRIu32 "\r\n", read_length, protobufSize);
+                    TRACE_WARNING("Invalid TAF-header on %s, read_length=%" PRIuSIZE " != protobufSize=%" PRIu32 "\r\n", contentPath, read_length, protobufSize);
                 }
             }
             else
             {
-                TRACE_WARNING("Invalid TAF-header, protobufSize=%" PRIu32 " >= TAF_HEADER_SIZE=%u\r\n", protobufSize, TAF_HEADER_SIZE);
+                TRACE_WARNING("Invalid TAF-header on %s, protobufSize=%" PRIu32 " >= TAF_HEADER_SIZE=%u\r\n", contentPath, protobufSize, TAF_HEADER_SIZE);
             }
+        }
+        else if (read_length == 0)
+        {
+            // TODO don't send invalid TAF files via API
+            TRACE_VERBOSE("Invalid TAF-header, file %s is empty!", contentPath);
         }
         else
         {
-            TRACE_WARNING("Invalid TAF-header, Could not read 4 bytes, read_length=%" PRIuSIZE "\r\n", read_length);
+            TRACE_WARNING("Invalid TAF-header on %s, Could not read 4 bytes, read_length=%" PRIuSIZE "\r\n", contentPath, read_length);
         }
         fsCloseFile(file);
     }
