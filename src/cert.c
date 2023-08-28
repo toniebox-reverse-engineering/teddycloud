3e11129b6da8de4eae56c08404105064f188f42a
g3gg0 <git@g3gg0.de>
2023-08-29 01:13:25 +0200

    rename variables

--- src/cert.c
+++ src/cert.c
@@ -37,19 +37,6 @@ static void hex_string_to_bytes(const char *hex_string, uint8_t *output)
 
 int cert_generate(const char *mac, const char *dest)
 {
-    /* create needed structures */
-    X509CertRequestInfo cert_req;
-    X509SerialNumber serial;
-    X509Validity validity;
-    X509SignAlgoId algo;
-    X509CertInfo issuerCertInfo;
-
-    osMemset(&cert_req, 0x00, sizeof(cert_req));
-    osMemset(&serial, 0x00, sizeof(serial));
-    osMemset(&validity, 0x00, sizeof(validity));
-    osMemset(&algo, 0x00, sizeof(algo));
-    osMemset(&issuerCertInfo, 0x00, sizeof(issuerCertInfo));
-
     /*********************************************/
     /*         load server CA certificate        */
     /*********************************************/
@@ -71,17 +58,19 @@ int cert_generate(const char *mac, const char *dest)
         return -1;
     }
 
-    if (x509ParseCertificateEx(server_ca_der, server_ca_der_size, &issuerCertInfo, true) != NO_ERROR)
+    X509CertInfo issuer_certinfo;
+    osMemset(&issuer_certinfo, 0x00, sizeof(issuer_certinfo));
+    if (x509ParseCertificateEx(server_ca_der, server_ca_der_size, &issuer_certinfo, true) != NO_ERROR)
     {
         TRACE_ERROR("x509ParseCertificateEx failed\r\n");
         return -1;
     }
 
-    RsaPrivateKey caPrivateKey;
-    osMemset(&caPrivateKey, 0x00, sizeof(caPrivateKey));
+    RsaPrivateKey server_ca_priv;
+    osMemset(&server_ca_priv, 0x00, sizeof(server_ca_priv));
 
     TRACE_INFO("Load CA key...\r\n");
-    if (pemImportRsaPrivateKey(server_key, osStrlen(server_key), NULL, &caPrivateKey) != NO_ERROR)
+    if (pemImportRsaPrivateKey(server_key, osStrlen(server_key), NULL, &server_ca_priv) != NO_ERROR)
     {
         TRACE_ERROR("pemImportRsaPrivateKey failed\r\n");
         return -1;
@@ -91,24 +80,24 @@ int cert_generate(const char *mac, const char *dest)
     /* now generate a RSA key for the new client */
     /*********************************************/
     TRACE_INFO("Generating RSA Key...\r\n");
-    RsaPrivateKey privateKey;
-    RsaPublicKey publicKey;
-    osMemset(&privateKey, 0x00, sizeof(privateKey));
-    osMemset(&publicKey, 0x00, sizeof(publicKey));
-    if (rsaGenerateKeyPair(YARROW_PRNG_ALGO, &yarrowContext, 4096, 65537, &privateKey, &publicKey) != NO_ERROR)
+    RsaPrivateKey cert_privkey;
+    RsaPublicKey cert_pubkey;
+    osMemset(&cert_privkey, 0x00, sizeof(cert_privkey));
+    osMemset(&cert_pubkey, 0x00, sizeof(cert_pubkey));
+    if (rsaGenerateKeyPair(YARROW_PRNG_ALGO, &yarrowContext, 4096, 65537, &cert_privkey, &cert_pubkey) != NO_ERROR)
     {
         TRACE_ERROR("rsaGenerateKeyPair failed\r\n");
         return -1;
     }
 
     size_t privateKey_der_size = 0;
-    if (x509ExportRsaPrivateKey(&privateKey, NULL, &privateKey_der_size) != NO_ERROR)
+    if (x509ExportRsaPrivateKey(&cert_privkey, NULL, &privateKey_der_size) != NO_ERROR)
     {
         TRACE_ERROR("x509ExportRsaPrivateKey failed\r\n");
         return -1;
     }
-    uint8_t *privateKey_der_data = osAllocMem(privateKey_der_size);
-    if (x509ExportRsaPrivateKey(&privateKey, privateKey_der_data, &privateKey_der_size) != NO_ERROR)
+    uint8_t *der_data = osAllocMem(privateKey_der_size);
+    if (x509ExportRsaPrivateKey(&cert_privkey, der_data, &privateKey_der_size) != NO_ERROR)
     {
         TRACE_ERROR("x509ExportRsaPrivateKey failed\r\n");
         return -1;
@@ -118,64 +107,66 @@ int cert_generate(const char *mac, const char *dest)
     char_t subj[32];
     osSprintf(subj, "b'%s'", mac);
 
+    X509CertRequestInfo cert_req;
+    osMemset(&cert_req, 0x00, sizeof(cert_req));
     cert_req.version = X509_VERSION_1;
     cert_req.subject.commonName.value = subj;
     cert_req.subject.commonName.length = osStrlen(subj);
     cert_req.subjectPublicKeyInfo.oid.value = RSA_ENCRYPTION_OID;
     cert_req.subjectPublicKeyInfo.oid.length = sizeof(RSA_ENCRYPTION_OID);
 
-    cert_req.subjectPublicKeyInfo.rsaPublicKey.e.length = mpiGetByteLength(&publicKey.e);
-    cert_req.subjectPublicKeyInfo.rsaPublicKey.n.length = mpiGetByteLength(&publicKey.n);
+    cert_req.subjectPublicKeyInfo.rsaPublicKey.e.length = mpiGetByteLength(&cert_pubkey.e);
+    cert_req.subjectPublicKeyInfo.rsaPublicKey.n.length = mpiGetByteLength(&cert_pubkey.n);
     uint8_t *rsa_e_buf = osAllocMem(cert_req.subjectPublicKeyInfo.rsaPublicKey.e.length);
     uint8_t *rsa_n_buf = osAllocMem(cert_req.subjectPublicKeyInfo.rsaPublicKey.n.length);
     cert_req.subjectPublicKeyInfo.rsaPublicKey.e.value = rsa_e_buf;
     cert_req.subjectPublicKeyInfo.rsaPublicKey.n.value = rsa_n_buf;
-    mpiExport(&publicKey.e, rsa_e_buf, cert_req.subjectPublicKeyInfo.rsaPublicKey.e.length, MPI_FORMAT_BIG_ENDIAN);
-    mpiExport(&publicKey.n, rsa_n_buf, cert_req.subjectPublicKeyInfo.rsaPublicKey.n.length, MPI_FORMAT_BIG_ENDIAN);
+    mpiExport(&cert_pubkey.e, rsa_e_buf, cert_req.subjectPublicKeyInfo.rsaPublicKey.e.length, MPI_FORMAT_BIG_ENDIAN);
+    mpiExport(&cert_pubkey.n, rsa_n_buf, cert_req.subjectPublicKeyInfo.rsaPublicKey.n.length, MPI_FORMAT_BIG_ENDIAN);
 
     uint8_t ser[32];
     ser[0] = 0;
     hex_string_to_bytes(mac, &ser[1]);
+
+    X509SerialNumber serial;
+    osMemset(&serial, 0x00, sizeof(serial));
     serial.length = 7;
     serial.value = ser;
 
+    X509Validity validity;
+    osMemset(&validity, 0x00, sizeof(validity));
     getCurrentDate(&validity.notBefore);
     getCurrentDate(&validity.notAfter);
     validity.notBefore.year -= 20;
     validity.notAfter.year += 20;
 
+    X509SignAlgoId algo;
+    osMemset(&algo, 0x00, sizeof(algo));
     algo.oid.value = SHA256_WITH_RSA_ENCRYPTION_OID;
     algo.oid.length = sizeof(SHA256_WITH_RSA_ENCRYPTION_OID);
 
-    size_t derSize = 0;
-    TRACE_INFO("Generating Certificate...\r\n");
-    if (x509CreateCertificate(YARROW_PRNG_ALGO, &yarrowContext, &cert_req, NULL, &issuerCertInfo, &serial, &validity, &algo, &caPrivateKey, NULL, &derSize) != NO_ERROR)
+    size_t cert_der_size = 8192;
+    uint8_t *cert_der = osAllocMem(cert_der_size);
+    if (x509CreateCertificate(YARROW_PRNG_ALGO, &yarrowContext, &cert_req, NULL, &issuer_certinfo, &serial, &validity, &algo, &server_ca_priv, cert_der, &cert_der_size) != NO_ERROR)
     {
         TRACE_ERROR("x509CreateCertificate failed\r\n");
         return -1;
     }
 
-    uint8_t *derOutput = osAllocMem(derSize);
-    if (x509CreateCertificate(YARROW_PRNG_ALGO, &yarrowContext, &cert_req, NULL, &issuerCertInfo, &serial, &validity, &algo, &caPrivateKey, derOutput, &derSize) != NO_ERROR)
-    {
-        TRACE_ERROR("x509CreateCertificate failed\r\n");
-        return -1;
-    }
-
-    size_t pemSize;
-    if (pemExportCertificate(derOutput, derSize, NULL, &pemSize) != NO_ERROR)
+    size_t cert_pem_size;
+    if (pemExportCertificate(cert_der, cert_der_size, NULL, &cert_pem_size) != NO_ERROR)
     {
         TRACE_ERROR("pemExportCertificate failed\r\n");
         return -1;
     }
 
-    char_t *pemOutput = osAllocMem(pemSize + 1);
-    if (pemExportCertificate(derOutput, derSize, pemOutput, &pemSize) != NO_ERROR)
+    char_t *cert_pem = osAllocMem(cert_pem_size + 1);
+    if (pemExportCertificate(cert_der, cert_der_size, cert_pem, &cert_pem_size) != NO_ERROR)
     {
         TRACE_ERROR("pemExportCertificate failed\r\n");
         return -1;
     }
-    pemOutput[pemSize] = 0;
+    cert_pem[cert_pem_size] = 0;
 
     /* save the cert as pem */
     {
@@ -188,7 +179,7 @@ int cert_generate(const char *mac, const char *dest)
             TRACE_ERROR("fsOpenFile failed\r\n");
             return -1;
         }
-        fsWriteFile(file, pemOutput, pemSize);
+        fsWriteFile(file, cert_pem, cert_pem_size);
         fsCloseFile(file);
         osFreeMem(path);
     }
@@ -204,7 +195,7 @@ int cert_generate(const char *mac, const char *dest)
             TRACE_ERROR("fsOpenFile failed\r\n");
             return -1;
         }
-        fsWriteFile(file, derOutput, derSize);
+        fsWriteFile(file, cert_der, cert_der_size);
         fsCloseFile(file);
         osFreeMem(path);
     }
@@ -220,20 +211,20 @@ int cert_generate(const char *mac, const char *dest)
             TRACE_ERROR("fsOpenFile failed\r\n");
             return -1;
         }
-        fsWriteFile(file, privateKey_der_data, privateKey_der_size);
+        fsWriteFile(file, der_data, privateKey_der_size);
         fsCloseFile(file);
         osFreeMem(path);
     }
 
-    rsaFreePublicKey(&publicKey);
-    rsaFreePrivateKey(&privateKey);
+    rsaFreePublicKey(&cert_pubkey);
+    rsaFreePrivateKey(&cert_privkey);
 
     osFreeMem(server_ca_der);
-    osFreeMem(derOutput);
+    osFreeMem(cert_der);
     osFreeMem(rsa_n_buf);
     osFreeMem(rsa_e_buf);
-    osFreeMem(privateKey_der_data);
-    osFreeMem(pemOutput);
+    osFreeMem(der_data);
+    osFreeMem(cert_pem);
 
     return 0;
 }
