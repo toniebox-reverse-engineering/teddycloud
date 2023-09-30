6e9e4462b85f316d54fe7f83e4bffe3fc6f6b1f3
g3gg0 <git@g3gg0.de>
2023-09-30 21:06:19 +0200

    fix docker entrypoint script

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,21 +5,11 @@ set -o pipefail
 set -o nounset
 # set -o xtrace
 
-#cd /teddycloud/
-
-#if [ ! -f "/teddycloud/certs/server/ca.der" ]; then
-#  echo "Creating certs..."
-#  gencerts.sh
-#  if [ ! -f "/teddycloud/certs/server/ca.der" ]; then
-#    echo "Error during certs creation"
-#    exit
-#  fi
-#fi
-
 curl -f https://gt-blog.de/JSON/tonies.json?source=teddyCloud-docker -o /teddycloud/config/tonies.json || true
 
 while true
 do
+  cd /teddycloud
   teddycloud
   retVal=$?
   if [ $retVal -ne -2 ]; then
