395b375e24eb2093030aeafd6038477f3c4f0de6
ivan Chubb <ivan.a.chubb.mil@army.mil>
2023-09-30 15:41:33 -0400

    adds some error handling, cleans up some magic values

--- src/main.c
+++ src/main.c
@@ -7,11 +7,6 @@
 #include <stdint.h>
 #include <math.h>
 
-#ifdef WIN32
-#else
-#include <unistd.h>
-#endif
-
 #include "error.h"
 #include "debug.h"
 #include "cJSON.h"
@@ -28,11 +23,23 @@
 #include "cert.h"
 #include "toniefile.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#define PATH_LEN MAX_PATH
+#else
+#include <unistd.h>
+#include <limits.h>
+#define PATH_LEN PATH_MAX
+#endif
+
+#define DEFAULT_HTTP_PORT 80
+#define DEFAULT_HTTPS_PORT 443
+#define PORT_MAX 65535
+
 void platform_init(void);
 void platform_deinit(void);
 void server_init(void);
-#define DEFAULT_HTTP_PORT 80
-#define DEFAULT_HTTPS_PORT 443
+static char *get_cwd(char *buffer, size_t size);
 
 typedef enum
 {
@@ -40,29 +47,6 @@ typedef enum
     PROT_HTTPS
 } Protocol;
 
-void get_directory_path(const char *filepath, char *dirpath, int maxLen)
-{
-    // Find the last occurrence of '/' or '\' in the file path
-    int lastSlash = -1;
-    for (int i = 0; filepath[i] != '\0'; i++)
-    {
-        if (filepath[i] == '/' || filepath[i] == '\\')
-        {
-            lastSlash = i;
-        }
-    }
-
-    if (lastSlash == -1)
-    {
-        // No directory part found, use an empty string for the directory path
-        dirpath[0] = '\0';
-    }
-    else
-    {
-        // Copy the characters before the last slash to the directory path buffer
-        snprintf(dirpath, maxLen, "%.*s", lastSlash, filepath);
-    }
-}
 
 bool parse_url(const char *url, char **hostname, uint16_t *port, char **uri, Protocol *protocol)
 {
@@ -98,7 +82,16 @@ bool parse_url(const char *url, char **hostname, uint16_t *port, char **uri, Pro
         strncpy(*hostname, url, hostname_length);
         (*hostname)[hostname_length] = '\0';
 
-        *port = (uint16_t)atoi(port_start + 1);
+        // ensures port is in a valid range before casting
+        long temp = strtol(port_start + 1, NULL, 10);
+        if ((temp >= 0) && (temp <= PORT_MAX))
+        {
+            *port = (uint16_t)temp;
+        }
+        else
+        {
+            *port = (*protocol == PROT_HTTP) ? DEFAULT_HTTP_PORT : DEFAULT_HTTPS_PORT;
+        }
     }
     else
     {
@@ -122,14 +115,17 @@ int_t main(int argc, char *argv[])
 
     error_t error = 0;
 
-    char cwd[256];
-    if (getcwd(cwd, sizeof(cwd)) == NULL)
+    char *cwd = calloc(PATH_LEN, sizeof(char));
+
+    if ((cwd == NULL) || (get_cwd(cwd, PATH_LEN) == NULL))
     {
-        get_directory_path(argv[0], cwd, sizeof(cwd));
+        free(cwd);
+        return -1;
     }
 
     /* platform specific init */
     error = settings_init(cwd);
+    free(cwd);
     if (error != NO_ERROR)
     {
         TRACE_ERROR("settings_init() failed with error code %d\r\n", error);
@@ -350,4 +346,13 @@ int_t main(int argc, char *argv[])
     settings_deinit_all();
 
     return error;
+}
+
+static char *get_cwd(char *buffer, size_t size)
+{
+#ifdef _WIN32
+    return _getcwd(buffer, size);
+#else
+    return getcwd(buffer, size);
+#endif
 }
