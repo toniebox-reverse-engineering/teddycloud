7240fd93f90fb6cc3ee9f71d53cad30928cd3147
pepe <git@pe82.de>
2024-01-20 14:47:52 +0000

    Make path configs available in Makefile and get rid of runtime OS detection for the default.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,13 +8,11 @@ set -o nounset
 mkdir -p /teddycloud/certs/server /teddycloud/certs/client
 
 if [ -n "${DOCKER_TEST:-}" ]; then
-  cd /teddycloud
   LSAN_OPTIONS=detect_leaks=0 teddycloud DOCKER_TEST
 else
   while true
   do
     #curl -f https://raw.githubusercontent.com/toniebox-reverse-engineering/tonies-json/release/tonies.json -o /teddycloud/config/tonies.json || true
-    cd /teddycloud
     teddycloud
     retVal=$?
     if [ $retVal -ne -2 ]; then
--- src/main.c
+++ src/main.c
@@ -123,23 +123,7 @@ int_t main(int argc, char *argv[])
         return -1;
     }
 
-    /* platform specific init */
     char *base_path = strdup(BASE_PATH);
-    if (osStrlen(base_path) == 0)
-    {
-        char *linux_usrlocaletc = "/usr/local/etc/teddycloud";
-        char *linux_etc = "/etc/teddycloud";
-        if (fsDirExists(linux_usrlocaletc))
-        {
-            free(base_path);
-            base_path = strdup(linux_usrlocaletc);
-        }
-        else if (fsDirExists(linux_etc))
-        {
-            free(base_path);
-            base_path = strdup(linux_etc);
-        }
-    }
 
     error = settings_init(cwd, base_path); // TODO: dynamicly read from cli
     free(base_path);
