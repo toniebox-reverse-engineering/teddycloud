3949de2a2c0db91d4a10db172c16132f7f03a58b
SciLor <scilor@users.noreply.github.com>
2024-04-05 19:04:58 +0000

    fix path traversal

--- src/handler.c
+++ src/handler.c
@@ -45,7 +45,19 @@ void cbrCloudOtaHeader(void *src_ctx, HttpClientContext *cloud_ctx, const char *
                 char *prefix = "attachment;filename=";
                 if (osStrncmp(value, prefix, osStrlen(prefix)) == 0)
                 {
-                    const char *filename = &value[osStrlen(prefix)]; // TODO path traversal
+                    const char *filename = &value[osStrlen(prefix)];
+                    // search for PATH_SERPERATOR_LINUX / PATH_SERPERATOR_LINUX and replace with null
+                    char *p1 = osStrchr(filename, PATH_SEPARATOR_LINUX);
+                    char *p2 = osStrchr(filename, PATH_SEPARATOR_WINDOWS);
+                    if (p1)
+                    {
+                        *p1 = '\0';
+                    }
+                    if (p2)
+                    {
+                        *p2 = '\0';
+                    }
+
                     char *folder;
                     switch (ctx->client_ctx->settings->internal.toniebox_firmware.boxIC)
                     {
